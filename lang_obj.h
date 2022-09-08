ae4d470113d87255586c783324ef30f42ec37666
Erick Augusto <erickaugusto542@gmail.com>
2022-09-07 20:55:42 -0500

    `Lang_obj` and `Data`

--- lang_obj.h
+++ lang_obj.h
@@ -2,6 +2,14 @@
 #define LANG_OBJ 1
 
 typedef struct Lang_obj {
+  char *name;
+  char *value;
+  struct Lang_obj *previous_lang_obj;
 } Lang_obj;
+
+/* A stack that stores every `Lang_obj`. */
+typedef struct Data {
+  Lang_obj *top;
+} Data;
 
 #endif /* lang_obj.h */
