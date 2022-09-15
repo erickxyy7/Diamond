023e72433928bcb01f3d5fa3d429dbec0f9112c2
Erick Augusto <erickaugusto542@gmail.com>
2022-09-15 19:40:26 +0000

    Bug fixes and functions documentation.

--- interpreter.h
+++ interpreter.h
@@ -13,32 +13,32 @@
 #include "output/print_number_to_standard_output.h"
 
 void interpreter(char **tokens, size_t tokens_length) {
-  
+
   Data *data = malloc(sizeof *data);
   initialize__Data(data);
-  
+
   for(size_t i = 0; i < tokens_length; ++i) {
-    
+
     if (!strcmp(tokens[i], "=")) {
-      
+
       size_t name = i - 1;
-      
+
       char **expression = NULL;
-      
+
       size_t j = 0;
       while (strcmp(tokens[++i], ";")) {
-        
+
         expression = realloc(expression, sizeof expression * j + 1);
         expression[j] = malloc(sizeof expression[j] * strlen(tokens[i]) + 1);
-        
+
         strcpy(expression[j++], tokens[i]);
-        
+
       }
-      
+
       char *result = postfix_evaluator(data, expression, j);
-      
+
       Lang_obj *lang_obj = name_exists(data, tokens[name]);
-      
+
       if (!lang_obj) {
         lang_obj = create_Lang_obj(tokens[name], result);
         push__Data(data, lang_obj);
@@ -46,96 +46,64 @@ void interpreter(char **tokens, size_t tokens_length) {
         lang_obj->value = realloc(lang_obj->value, sizeof (char) * strlen(result) + 1);
         strcpy(lang_obj->value, result);
       }
-      
+
       free(result);
-      
+
       for(size_t i = 0; i < j; ++i)
         free(expression[i]);
       free(expression);
     }
-    
+
     else if (!strcmp(tokens[i], "puts")) {
-      
+
       char **expression = NULL;
-      
+
       size_t j = 0;
       while (strcmp(tokens[++i], ";")) {
-        
+
         expression = realloc(expression, sizeof expression * j + 1);
         expression[j] = malloc(sizeof expression[j] * strlen(tokens[i]) + 1);
-        
+
         strcpy(expression[j++], tokens[i]);
-        
+
       }
-      
+
       char *result = postfix_evaluator(data, expression, j);
-      
+
       for(size_t i = 0; i < j; ++i)
         free(expression[i]);
       free(expression);
-      
+
       if (is_number(result))
         print_number_to_standard_output(result);
       else if (is_string(result))
         print_string_to_standard_output(result);
-      
+
       free(result);
     }
-    
+
     else if (!strcmp(tokens[i], "while")) {
-      
+
       char **expression = NULL;
-      
+
       size_t j = 0;
       while (strcmp(tokens[++i], ";")) {
         expression = realloc(expression, sizeof expression * j + 1);
         expression[j] = malloc(sizeof expression[j] * strlen(tokens[i]) + 1);
         strcpy(expression[j++], tokens[i]);
       }
-      
+
       char *result = postfix_evaluator(data, expression, j);
-      
+
       if (!is_true(result)) {
         int gauge = 0;
         while (true) {
-          
+
           if (!strcmp(tokens[i], "while"))
             ++gauge;
-          else if (!strcmp(tokens[i], "end")) {
-            if (gauge == 0) {
-              ++i;
-              break;
-            }
-            --gauge;
-          }
-          
-          ++i;
-        }
-      }
-      
-      for(size_t i = 0; i < j; ++i)
-        free(expression[i]);
-      free(expression);
-      free(result);
-    }
-    
-    else if (!strcmp(tokens[i], "if")) {
-      char **expression = NULL;
-      
-      size_t j = 0;
-      while (strcmp(tokens[++i], ";")) {
-        expression = realloc(expression, sizeof expression * j + 1);
-        expression[j] = malloc(sizeof expression[j] * strlen(tokens[i]) + 1);
-        strcpy(expression[j++], tokens[i]);
-      }
-      
-      char *result = postfix_evaluator(data, expression, j);
-      
-      if (!is_true(result)) {
-        int gauge = 0;
-        while (true) {
-          
-          if (!strcmp(tokens[i], "if"))
+          else if (!strcmp(tokens[i], "if"))
+            ++gauge;
+          else if (!strcmp(tokens[i], "fn"))
             ++gauge;
           else if (!strcmp(tokens[i], "end")) {
             if (gauge == 0) {
@@ -144,49 +112,121 @@ void interpreter(char **tokens, size_t tokens_length) {
             }
             --gauge;
           }
-          
+
           ++i;
         }
       }
-      
+
       for(size_t i = 0; i < j; ++i)
         free(expression[i]);
       free(expression);
       free(result);
     }
-    
+
+    else if (!strcmp(tokens[i], "if")) {
+      char **expression = NULL;
+
+      size_t j = 0;
+      while (strcmp(tokens[++i], ";")) {
+        expression = realloc(expression, sizeof expression * j + 1);
+        expression[j] = malloc(sizeof expression[j] * strlen(tokens[i]) + 1);
+        strcpy(expression[j++], tokens[i]);
+      }
+
+      char *result = postfix_evaluator(data, expression, j);
+
+      if (!is_true(result)) {
+        int gauge = 0;
+        while (true) {
+
+          if (!strcmp(tokens[i], "if"))
+            ++gauge;
+          else if (!strcmp(tokens[i], "while"))
+            ++gauge;
+          else if (!strcmp(tokens[i], "fn"))
+            ++gauge;
+          else if (!strcmp(tokens[i], "end")) {
+            if (gauge == 0) {
+              ++i;
+              break;
+            }
+            --gauge;
+          }
+
+          ++i;
+        }
+      }
+
+      for(size_t i = 0; i < j; ++i)
+        free(expression[i]);
+      free(expression);
+      free(result);
+    }
+
     else if (!strcmp(tokens[i], "end")) {
-      
+
       /* Finds the keyword this `end` is for. */
       int gauge = 0;
       int j = i - 1;
-      
+
       while (true) {
-        
+
         if (!strcmp(tokens[j], "end"))
           ++gauge;
-        
+
+        else if (!strcmp(tokens[j], "fn")) {
+          if (gauge == 0) {
+            puts("Fim de execução de função.");
+            exit(0);
+          }
+
+          --gauge;
+        }
+
         else if (!strcmp(tokens[j], "if")) {
           if (gauge == 0) {
             ++i;
             break;
           }
-          
+
           --gauge;
         }
-        
+
         else if (!strcmp(tokens[j], "while")) {
           if (gauge == 0) {
             i = --j;
             break;
           }
-          
+
           --gauge;
         }
-        
+
         --j;
       }
-      
+
+    }
+
+    else if (!strcmp(tokens[i], "fn")) {
+      /* Finds the `end` equivalent of this `fn` and skips to that `end`. */
+      int gauge = 0;
+      while (true) {
+        ++i;
+
+        if (!strcmp(tokens[i], "if"))
+          ++gauge;
+        else if (!strcmp(tokens[i], "while"))
+          ++gauge;
+        else if (!strcmp(tokens[i], "fn"))
+          ++gauge;
+        else if (!strcmp(tokens[i], "end")) {
+          if (gauge == 0) {
+            ++i;
+            break;
+          }
+
+          --gauge;
+        }
+      }
     }
   }
 }
